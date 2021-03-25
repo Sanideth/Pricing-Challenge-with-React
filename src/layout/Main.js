3d93c15b263d47e2da9ce6ae8f611bd8a646f887
Sandro Bochorishvili <Sanideth@gmail.com>
2021-03-26 00:01:33 +0400

    main initial markup and styling done

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -6,9 +6,33 @@ const Main = () => {
     <div className="main">
       <div className="container">
         <section className="main__price-cards">
-          <PriceCard />
-          <PriceCard />
-          <PriceCard />
+          <PriceCard
+            type="Basic"
+            annualPrice="199.99"
+            monthlyPrice="19.99"
+            storage="500 GB"
+            users="2"
+            bandwidth="3"
+            className="--left"
+          />
+          <PriceCard
+            type="Profesional"
+            annualPrice="249.99"
+            monthlyPrice="24.99"
+            storage="1 TB"
+            users="5"
+            bandwidth="10"
+            className="--center"
+          />
+          <PriceCard
+            type="Master"
+            annualPrice="399.99"
+            monthlyPrice="39.99"
+            storage="2 TB"
+            users="10"
+            bandwidth="20"
+            className="--right"
+          />
         </section>
       </div>
     </div>
