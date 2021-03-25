52d61141f672edb15d0d9e53b5cae9b8a4d40e81
Sandro Bochorishvili <Sanideth@gmail.com>
2021-03-25 23:59:41 +0400

    component for price card pretty much done

--- src/components/PriceCard.js
+++ src/components/PriceCard.js
@@ -1,16 +1,21 @@
 import React from "react";
-import ListParagraph from "./ListParagraph";
+import CardList from "./CardList";
 import ListButton from "./ListButton";
 
-const PriceCard = () => {
+const PriceCard = (props) => {
   return (
-    <div className="main__price-card">
-      <h2 className="heading-secondary">Something</h2>
-      <ul className="main__list">
-        <ListParagraph />
-        <ListParagraph />
-        <ListParagraph />
-      </ul>
+    <div className={`main__price-card main__price-card${props.className}`}>
+      <h3 className="heading-tertiary">{props.type}</h3>
+      <p className="main__price">
+        <span className="main__currency">&#36;</span> {props.annualPrice}
+      </p>
+
+      <CardList
+        storage={props.storage}
+        users={props.users}
+        bandwidth={props.bandwidth}
+      />
+
       <ListButton />
     </div>
   );
